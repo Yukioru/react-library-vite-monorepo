518859a93f5f710646f151965c9e45f9425b76ff
Andrey Kondalov <andrey@kondalov.com>
2024-01-10 01:44:48 +0400

    Change css bundler to LightningCSS without splitting and some improvements

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,26 @@ import { defineConfig } from 'vitest/config';
 import { glob } from 'glob';
 import react from '@vitejs/plugin-react-swc';
 import dts from 'vite-plugin-dts';
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
+
+import pkg from './package.json';
 
 const isProd = process.env.NODE_ENV === 'production';
-
-const libraryName = 'uikit';
-const scopeDevName = '[name]-[local]_[hash:base64:4]';
-const scopeProdName = `${libraryName}-[local]_[hash:base64:12]`;
+const browserslistConfig = 'last 3 versions, not dead';
+const scopeDevName = '[hash]_[name]_[local]';
+const scopeProdName = `${pkg.name}-[hash]_[local]`;
 const ignoreList = [
   'src/**/*.story.tsx',
   'src/**/*.test.tsx',
   'src/setupTest.ts',
+  'src/**/*.d.ts',
 ];
 
-// https://vitejs.dev/config/
+const jsTargets = browserslistToEsbuild(browserslistConfig);
+const cssTargets = browserslistToTargets(browserslist(browserslistConfig));
+
 export default defineConfig({
   resolve: {
     alias: {
@@ -31,34 +38,37 @@ export default defineConfig({
     }),
   ],
   css: {
-    modules: {
-      localsConvention: 'camelCaseOnly',
-      generateScopedName: isProd ? scopeProdName : scopeDevName,
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: cssTargets,
+      cssModules: {
+        pattern: isProd ? scopeProdName : scopeDevName,
+        dashedIdents: true,
+      },
     },
   },
   build: {
     copyPublicDir: false,
+    cssCodeSplit: false,
+    ssrEmitAssets: true,
+    cssMinify: 'lightningcss',
+    minify: isProd,
+    target: jsTargets,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
     },
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime', 'react-dom'],
+      external: ['react', 'react/jsx-runtime', 'react-dom', 'clsx'],
       input: Object.fromEntries(
         glob
           .sync('src/**/*.{ts,tsx}', {
             ignore: ignoreList,
           })
-          .map(file => {
-            console.log('file', file);
-            return [
-              relative(
-                'src',
-                file.slice(0, file.length - extname(file).length),
-              ),
-              fileURLToPath(new URL(file, import.meta.url)),
-            ];
-          }),
+          .map(file => [
+            relative('src', file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
       ),
       output: {
         assetFileNames: 'assets/[name][extname]',
