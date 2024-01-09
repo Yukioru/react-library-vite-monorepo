67e77411ac7f1df62c350e0d882350f81f3ddf5a
Andrey Kondalov <andrey@kondalov.com>
2024-01-09 22:38:48 +0400

    Remove css injection

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from 'vitest/config';
 import { glob } from 'glob';
 import react from '@vitejs/plugin-react-swc';
 import dts from 'vite-plugin-dts';
-import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -27,7 +26,6 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    libInjectCss(),
     dts({
       exclude: ignoreList,
     }),
